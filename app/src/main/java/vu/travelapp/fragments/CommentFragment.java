bb6bd839d0d3d3660b7d6615f500bb62e8b41787
MeoMunm <giahungtran1996@gmail.com>
2017-08-25 21:50:07 +0700

    custom lại toast khi notii được gửi đi và app trong trạng thái background thì sẽ hiện toast

--- app/src/main/java/vu/travelapp/fragments/CommentFragment.java
+++ app/src/main/java/vu/travelapp/fragments/CommentFragment.java
@@ -10,6 +10,7 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +18,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
--- app/src/main/java/vu/travelapp/networks/notifications_service/services/NotificationFirebaseService.java
+++ app/src/main/java/vu/travelapp/networks/notifications_service/services/NotificationFirebaseService.java
@@ -1,10 +1,24 @@
 package vu.travelapp.networks.notifications_service.services;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import vu.travelapp.R;
+import vu.travelapp.activities.MainActivity;
+
 
 /**
  * Created by ADMIN on 8/25/2017.
@@ -16,8 +30,40 @@ public class NotificationFirebaseService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        this.createNotification();
 
-        Toast.makeText(this, "Có thông báo mới", Toast.LENGTH_SHORT).show();
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            public void run() {
+           //     Toast.makeText(getApplicationContext(), "Toast on UI thread", Toast.LENGTH_SHORT).show();
+                final View viewToast = LayoutInflater.from(getApplicationContext()).inflate(R.layout.cus_toast, null);
+                ((TextView) viewToast.findViewById(R.id.tv_title)).setText("Thông báo");
+                ((TextView) viewToast.findViewById(R.id.tv_mess)).setText("Bạn có thông báo mới");
+                Toast toast = new Toast(getApplicationContext());
+                toast.setView(viewToast);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.TOP | Gravity.FILL_HORIZONTAL, 0, 0);
+                toast.show();
+            }
+        });
         Log.d(TAG, "onMessageReceived: new notification");
+    }
+
+    public void createNotification(){
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, intent, 0);
+
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentText("Thông báo từ Travel App");
+        builder.setContentText("Có thông bình luận mới chưa đọc");
+        builder.setSmallIcon(R.drawable.ic_share_white_24dp);
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(true);
+
+        notificationManager.notify(1, builder.build());
+
     }
 }
